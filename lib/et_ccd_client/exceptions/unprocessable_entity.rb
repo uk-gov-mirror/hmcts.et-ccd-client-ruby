74b41b10d556e90237e33775693102228075315d
Gary Taylor <gary.taylor@hismessages.com>
2019-07-31 11:34:42 +0100

    Bug fix - 422 error could fail if the data is not correct in the response

--- lib/et_ccd_client/exceptions/unprocessable_entity.rb
+++ lib/et_ccd_client/exceptions/unprocessable_entity.rb
@@ -5,7 +5,7 @@ module EtCcdClient
         json = JSON.parse(response.body) rescue JSON::JSONError
         return super if json.nil?
 
-        field_errors = json.dig('details', 'field_errors').map do |field_error|
+        field_errors = json.dig('details', 'field_errors')&.map do |field_error|
           "#{field_error['id']} => #{field_error['message']}"
         end
         return super if field_errors.nil?
